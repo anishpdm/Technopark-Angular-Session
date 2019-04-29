d8dec185bf69b89690cd1eb6cb1564a2bf827389
Anish S Nair <anish.vilayil.s@gmail.com>
2019-04-29 16:28:58 +0530

    angular app

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [NavbarComponent,MainComponent]
+})
+export class AppModule { }
